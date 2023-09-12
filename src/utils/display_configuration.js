3409e82f7d1a879b7db76558824312c67f198d77
Administrator <Administrator@172.31.36.201>
2023-09-12 11:53:57 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,48 @@
+export const inventoryViewConfig = {
+Details: [
+    {
+      key: "InventoryId",
+      value: "InventoryId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "ExpirtyDate",
+      value: "ExpirtyDate",
+      type: "date",
+      editable: false,
+      required: true
+    },
+    {
+      key: "PurchasedDate",
+      value: "PurchasedDate",
+      type: "date",
+      editable: false,
+      required: true
+    },
+    {
+      key: "VendorName",
+      value: "VendorName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "CurrentStockUnits",
+      value: "CurrentStockUnits",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "InStockUnits",
+      value: "InStockUnits",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
